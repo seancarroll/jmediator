5067ac8f502ffefa844b0872dda35b3cb6a12a42
seancarroll <seanc28@gmail.com>
2018-11-29 22:52:26 -0600

    Given this is a library I would like to try and avoid as many dependencies as possible, especially very popular ones such as Guava. I dont see the need at the moment to bring in Guava just for its Preconditions so this adds an Ensure to take the place of Guava's Preconditions

---
+++ jmediator-core/src/main/java/jmediator/Ensure.java
@@ -0,0 +1,18 @@
+package jmediator;
+
+public final class Ensure {
+
+    private Ensure() {
+        // static methods only
+    }
+
+    public static <T> T notNull(T argument) {
+        if (argument == null) {
+            throw new IllegalArgumentException("cannot be null");
+        }
+        return argument;
+    }
+
+
+
+}
