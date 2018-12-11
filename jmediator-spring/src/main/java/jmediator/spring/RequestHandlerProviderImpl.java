b32d1e36db89e7aec78d507dcbf25520b360677e
seancarroll <seanc28@gmail.com>
2018-12-10 22:46:57 -0600

    hold references to handler class names not actual RequestHandler instances. We should get instances from Spring's bean factory

--- jmediator-spring/src/main/java/jmediator/spring/RequestHandlerProviderImpl.java
+++ jmediator-spring/src/main/java/jmediator/spring/RequestHandlerProviderImpl.java
@@ -1,21 +1,21 @@
 package jmediator.spring;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 import jmediator.*;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // TODO: is there a way to do other scopes outside of singleton which is essentially is because you always
 // get back the same requesthandler.
-// TODO: Update name - the name is terrible. This is really a ComponentScanning request handler provider
-// or something like that
+// TODO: Update name - the name is terrible. This is really a ComponentScanning request handler provider or something like that
 // TODO: can we support Spring component scanning and manual registration that will adhere to scope?
 // TODO: can we support registering by class name?
+// TODO: allow users to validate that all request classes have associated handlers registered. Take a look at Marten
+// are there other projects that have a similar validation idea? Mediatr?
 // Will AutowireCapableBeanFactory help?
 // maybe we dont keep a cache and just always call the beanfactory?
 // what does spring do under the covers?
@@ -26,12 +26,17 @@ import org.springframework.context.event.ContextRefreshedEvent;
  * Default implementation that looks into the application event listener context
  * refreshed event and finds all beans implements RequestHandler
  *
+ *
+ * Default implementation that uses BeanFactory to find all registered RequestHandler beans.
+ *
+ * Is also an ApplicationListener that hooks into the ContextRefreshedEvent to find all registered RequestHandlers
+ * to create a map between Request classes and RequestHandler class names
+ *
  */
 public class RequestHandlerProviderImpl implements RequestHandlerProvider, ApplicationListener<ContextRefreshedEvent> {
 
     private ConfigurableListableBeanFactory beanFactory;
-    private Map<Class<?>, RequestHandler<Request, Object>> handlers = new HashMap<>();
-    private Map<Class<?>, String> h = new HashMap<>();
+    private Map<Class<?>, String> handlerClassNames = new HashMap<>();
 
     public RequestHandlerProviderImpl(ConfigurableListableBeanFactory beanFactory) {
         this.beanFactory = beanFactory;
@@ -39,21 +44,12 @@ public class RequestHandlerProviderImpl implements RequestHandlerProvider, Appli
 
     @Override
     public RequestHandler<Request, Object> getRequestHandler(Request request) {
-        RequestHandler<Request, Object> handler = handlers.get(request.getClass());
+        String handlerClassName = handlerClassNames.get(request.getClass());
+        RequestHandler<Request, Object> handler = beanFactory.getBean(handlerClassName, RequestHandler.class);
         if (handler == null) {
             throw new NoHandlerForRequestException("request handler not found for class " + request.getClass());
         }
         return handler;
-
-
-//        // if we dont cache the actual handler
-//        String beanName = h.get(request.getClass());
-//        // TODO: do we need to do a null check? If so can we do it here and would that guarantee handler would not be null?
-//        RequestHandler<Request, Object> handler = beanFactory.getBean(beanName, RequestHandler.class);
-//        if (handler == null) {
-//            throw new NoHandlerForRequestException("request handler not found for class " + request.getClass());
-//        }
-//        return handler;
     }
 
     /**
@@ -63,30 +59,19 @@ public class RequestHandlerProviderImpl implements RequestHandlerProvider, Appli
     @SuppressWarnings("unchecked")
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-        handlers.clear();
+        handlerClassNames.clear();
         String[] requestHandlersNames = beanFactory.getBeanNamesForType(RequestHandler.class);
         for (String beanName : requestHandlersNames) {
             try {
                 BeanDefinition requestHandler = beanFactory.getBeanDefinition(beanName);
                 Class<?> handlerClass = Class.forName(requestHandler.getBeanClassName());
                 Class<?> requestClass = ReflectionUtils.getTypeArgumentForGenericInterface(handlerClass, RequestHandler.class);
-                RequestHandler<Request, Object> handler = beanFactory.getBean(beanName, RequestHandler.class);
-                handlers.putIfAbsent(requestClass, handler);
-
-                // If we dont cache the actual handler
-                // would we still need the catch and throw here?
-//                BeanDefinition requestHandler = beanFactory.getBeanDefinition(beanName);
-//                Class<?> handlerClass = Class.forName(requestHandler.getBeanClassName());
-//                Class<?> requestClass = ReflectionUtils.getTypeArgumentForGenericInterface(handlerClass, RequestHandler.class);
-//                h.putIfAbsent(requestClass, beanName);
-
+                // we only want to store the class name as the actual handler should be managed by Spring and could have
+                // custom lifecycle or scope depending on how it added to the injection binder
+                handlerClassNames.putIfAbsent(requestClass, beanName);
             } catch (ClassNotFoundException e) {
                 throw new NoHandlerForRequestException("request handler not found for class " + beanName, e);
             }
         }
-    }
-
-    Map<Class<?>, RequestHandler<Request, Object>> getHandlers() {
-        return Collections.unmodifiableMap(handlers);
     }
 }
