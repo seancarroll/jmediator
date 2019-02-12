8ed954460233393ec15f6f6873ff119a51c9315d
seancarroll <seanc28@gmail.com>
2019-02-11 20:37:23 -0600

    exclude junit 4 and fix test

--- jmediator-samples/jmediator-sample-spring/src/test/java/jmediator/sample/spring/SampleSpringControllerTest.java
+++ jmediator-samples/jmediator-sample-spring/src/test/java/jmediator/sample/spring/SampleSpringControllerTest.java
@@ -1,10 +1,8 @@
 package jmediator.sample.spring;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.inject.Inject;
@@ -13,7 +11,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class SampleSpringControllerTest {
