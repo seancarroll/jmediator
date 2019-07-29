cf0922974250e0435cd02a8341f58a5f56f7dbae
seancarroll <seanc28@gmail.com>
2019-07-28 19:33:15 -0500

    adds another constructor to NoHandlerForRequestException that takes in a type and constructors a message. This remove need to duplicate error message

--- jmediator-core/src/main/java/jmediator/NoHandlerForRequestException.java
+++ jmediator-core/src/main/java/jmediator/NoHandlerForRequestException.java
@@ -8,6 +8,10 @@ public class NoHandlerForRequestException extends RuntimeException {
 
 	private static final long serialVersionUID = -3378587911169482266L;
 
+	public NoHandlerForRequestException(Class<?> type) {
+        super("request handler not found for class " + type);
+    }
+
 	public NoHandlerForRequestException(String message) {
 		super(message);
 	}
