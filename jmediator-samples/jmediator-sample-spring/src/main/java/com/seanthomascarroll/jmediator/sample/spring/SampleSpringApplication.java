58c216f6ac32716b03a24b0ef1863c64ee05ba4c
seancarroll <seanc28@gmail.com>
2020-05-03 20:19:27 -0500

    spring example should use springboot autoconfiguration

--- jmediator-samples/jmediator-sample-spring/src/main/java/com/seanthomascarroll/jmediator/sample/spring/SampleSpringApplication.java
+++ jmediator-samples/jmediator-sample-spring/src/main/java/com/seanthomascarroll/jmediator/sample/spring/SampleSpringApplication.java
@@ -1,29 +1,13 @@
 package com.seanthomascarroll.jmediator.sample.spring;
 
-import com.seanthomascarroll.jmediator.RequestDispatcher;
-import com.seanthomascarroll.jmediator.RequestDispatcherImpl;
-import com.seanthomascarroll.jmediator.RequestHandlerProvider;
-import com.seanthomascarroll.jmediator.spring.ComponentScanningRequestHandlerProvider;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SampleSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SampleSpringApplication.class, args);
-    }
-
-    @Bean
-    public RequestDispatcher requestDispatcher(RequestHandlerProvider requestHandlerProvider) {
-        return new RequestDispatcherImpl(requestHandlerProvider);
-    }
-
-    @Bean
-    public ComponentScanningRequestHandlerProvider requestHandlerProvider(ConfigurableListableBeanFactory beanFactory) {
-        return new ComponentScanningRequestHandlerProvider(beanFactory);
     }
 
 }
