3d81ba49d81623751f814e2f20ed937aede25d95
seancarroll <seanc28@gmail.com>
2020-05-24 13:07:40 -0500

    bit more polish around honeycomb pipeline behaviors

--- jmediator-honeycomb/src/main/java/com/seanthomascarroll/jmediator/pipeline/honeycomb/BeelineTraceBehavior.java
+++ jmediator-honeycomb/src/main/java/com/seanthomascarroll/jmediator/pipeline/honeycomb/BeelineTraceBehavior.java
@@ -8,12 +8,12 @@ import io.honeycomb.beeline.tracing.Span;
 import io.honeycomb.libhoney.HoneyClient;
 import io.honeycomb.libhoney.LibHoney;
 
+/**
+ *
+ */
 public class BeelineTraceBehavior implements PipelineBehavior {
 
     private final Beeline beeline;
-
-    // https://github.com/honeycombio/examples/blob/master/java-webapp/src/main/java/io/honeycomb/libhoney/example/webapp/instrumentation/HoneycombContext.java
-    // private final HoneycombContext honeycombContext;
 
     public BeelineTraceBehavior(Beeline beeline) {
         this.beeline = beeline;
@@ -21,9 +21,7 @@ public class BeelineTraceBehavior implements PipelineBehavior {
 
     @Override
     public <T extends Request> Object handle(T request, PipelineChain chain) {
-        // beeline.getActiveSpan().addField("request", request.getClass().getName());
         try (Span childSpan = beeline.startChildSpan(request.getClass().getName())) {
-            // childSpan.addField("request", request.getClass().getName());
             return chain.doBehavior();
         }
     }
--- jmediator-honeycomb/src/main/java/com/seanthomascarroll/jmediator/pipeline/honeycomb/HoneycombContext.java
+++ jmediator-honeycomb/src/main/java/com/seanthomascarroll/jmediator/pipeline/honeycomb/HoneycombContext.java
@@ -3,23 +3,18 @@ package com.seanthomascarroll.jmediator.pipeline.honeycomb;
 import io.honeycomb.libhoney.Event;
 import io.honeycomb.libhoney.HoneyClient;
 
-import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
-// @RequestScope
-// @Component
-
 /**
- * TODO: details
+ * Holds a Honeycomb {@link Event} and map of contextual data which can be passed around
+ * to components so that they can add to the end which will ultimately be sent to Honeycomb.
  *
- * Commonly used as a request scoped bean / per request basis
+ * This class should be a tied to a single HTTP request or a request scoped bean
+ *
  */
 public class HoneycombContext {
-//    @Autowired
-//    private HoneyClient honeyClient;
 
-    private HoneyClient honeyClient;
     private final Map<String, Object> contextualData;
     private final Event event;
 
@@ -27,12 +22,6 @@ public class HoneycombContext {
         this.event = honeyClient.createEvent();
         this.contextualData = new HashMap<>();
     }
-
-//    @PostConstruct
-//    public void setup() {
-//        this.event = honeyClient.createEvent();
-//        this.contextualData = new HashMap<>();
-//    }
 
     public Map<String, Object> getContextualData() {
         return contextualData;
