4f44563d48f3f66fdd3f696e700f34ed7932e326
seancarroll <seanc28@gmail.com>
2020-06-07 00:39:28 -0500

    adds additional tests to ClasspathScanningServiceFactory

--- jmediator-spring/src/test/java/com/seanthomascarroll/jmediator/spring/ClasspathScanningServiceFactoryTest.java
+++ jmediator-spring/src/test/java/com/seanthomascarroll/jmediator/spring/ClasspathScanningServiceFactoryTest.java
@@ -2,18 +2,28 @@ package com.seanthomascarroll.jmediator.spring;
 
 import com.seanthomascarroll.jmediator.NoHandlerForRequestException;
 import com.seanthomascarroll.jmediator.RequestHandler;
+import com.seanthomascarroll.jmediator.ServiceFactoryException;
+import com.seanthomascarroll.jmediator.pipeline.PipelineBehavior;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.FatalBeanException;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.inject.Inject;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = TestConfig.class)
@@ -61,4 +71,25 @@ class ClasspathScanningServiceFactoryTest {
 
         assertThrows(NoHandlerForRequestException.class, () -> serviceFactory.getRequestHandler(NotBean.class));
     }
+
+    @Test
+    void shouldRegisterPipelineBehavior() {
+        ClasspathScanningServiceFactory serviceFactory = new ClasspathScanningServiceFactory(beanFactory);
+
+        List<PipelineBehavior> behaviors = serviceFactory.getPipelineBehaviors();
+
+        assertEquals(1, behaviors.size());
+        assertTrue(behaviors.get(0) instanceof NoopPipelineBehavior);
+    }
+
+    @Test
+    void shouldThrowWhenPipelineBehaviorBeanCannotBeInstantiated() {
+        ConfigurableListableBeanFactory mockBeanFactory = mock(ConfigurableListableBeanFactory.class);
+        when(mockBeanFactory.getBeansOfType(PipelineBehavior.class)).thenThrow(FatalBeanException.class);
+
+        ClasspathScanningServiceFactory serviceFactory = new ClasspathScanningServiceFactory(mockBeanFactory);
+
+        assertThrows(ServiceFactoryException.class, () -> serviceFactory.getPipelineBehaviors());
+    }
+
 }
---
+++ jmediator-spring/src/test/java/com/seanthomascarroll/jmediator/spring/NoopPipelineBehavior.java
@@ -0,0 +1,15 @@
+package com.seanthomascarroll.jmediator.spring;
+
+import com.seanthomascarroll.jmediator.Request;
+import com.seanthomascarroll.jmediator.pipeline.PipelineBehavior;
+import com.seanthomascarroll.jmediator.pipeline.PipelineChain;
+
+import javax.inject.Named;
+
+@Named
+public class NoopPipelineBehavior implements PipelineBehavior {
+    @Override
+    public <T extends Request> Object handle(T request, PipelineChain chain) {
+        return chain.doBehavior();
+    }
+}
