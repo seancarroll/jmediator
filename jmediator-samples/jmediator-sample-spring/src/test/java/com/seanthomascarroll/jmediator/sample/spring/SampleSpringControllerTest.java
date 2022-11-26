bb17ec832c07bba7b364826318f78c7c8be4a703
seancarroll <seanc28@gmail.com>
2022-11-25 19:26:41 -0600

    move to springboot 3

--- jmediator-samples/jmediator-sample-spring/src/test/java/com/seanthomascarroll/jmediator/sample/spring/SampleSpringControllerTest.java
+++ jmediator-samples/jmediator-sample-spring/src/test/java/com/seanthomascarroll/jmediator/sample/spring/SampleSpringControllerTest.java
@@ -1,6 +1,6 @@
 package com.seanthomascarroll.jmediator.sample.spring;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
